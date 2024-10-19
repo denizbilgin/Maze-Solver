2f769cb5309a0525c67fddc6dda98f4dacbed6a1
Deniz Bilgin <denizbilgin156@gmail.com>
2024-10-20 02:14:34 +0300

    bug fix

--- HeuristicFringe.py
+++ HeuristicFringe.py
@@ -5,26 +5,25 @@ from Node import Node
 
 
 class HeuristicFringe(Fringe, ABC):
+    __distance_types = ["manhattan", "euclidean", "diagonal"]
+
     def __init__(self):
         super().__init__()
         self.goal: Union[Tuple[int, int], None] = None
         self.distance_type: Union[str, None] = None
 
-    def contains_state(self, state) -> bool:
-        result: bool = any(node.state == state for node in self.fringe)
-        return result
-
     def set_goal(self, goal: Tuple[int, int]):
         self.goal = goal
 
     def set_distance_type(self, distance_type: str):
-        if distance_type not in ["manhattan", "euclidean", "diagonal"]:
-            raise ValueError(f"Unknown distance type: {distance_type}")
+        distance_type = distance_type.lower().strip()
+        if distance_type not in self.__distance_types:
+            raise ValueError(f"Unknown distance type: {distance_type}. You can use these distances: {self.__distance_types}")
         self.distance_type = distance_type
 
     def distance(self, node: Node) -> float:
         if self.goal is None:
-            raise ValueError("Goal is not set.")
+            raise ValueError("Goal is not set. You need to set goal first.")
 
         current_state = node.state
         x1, y1 = current_state
@@ -37,4 +36,4 @@ class HeuristicFringe(Fringe, ABC):
         elif self.distance_type == "diagonal":
             return max(abs(x1 - x2), abs(y1 - y2))
         else:
-            raise ValueError(f"Unknown distance type: {self.distance_type}")
+            return -1
